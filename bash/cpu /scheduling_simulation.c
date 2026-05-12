875962ec249a62c1b53eb01ecd3a7ba3079bd3fc
abdmalikidm-hue <abdmalikidm@gmail.com>
2026-05-12 09:32:03 +0000

    Create scheduling_simulation.c

---
+++ bash/cpu /scheduling_simulation.c
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Initial Round Robin Scheduling Simulation
+processes = [("P1", 0, 5), ("P2", 1, 3), ("P3", 2, 4)]
+quantum = 2
+print("Round Robin Simulation:")
+# ... TODO: implement the algorithm
+print("Simulation ended.")
